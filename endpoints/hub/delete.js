7a777ecb551da69fd90ce0626a1750750ffa6395
MoaufmKlo <45636897+MoaufmKlo@users.noreply.github.com>
2022-03-03 17:30:47 +0100

    feat(hub): delete products on hub deletion

--- endpoints/hub/delete.js
+++ endpoints/hub/delete.js
@@ -5,6 +5,7 @@ module.exports = {
   delete: async (req, res) => {
     if (!req.hub) return res.status(401).send({ message: 'Unauthorized' }) // auth
 
+    await db.collection('products').deleteMany({ hub: req.hub._id })
     await db.collection('hubs').deleteOne(req.hub)
 
     return res.status(200).send({})
